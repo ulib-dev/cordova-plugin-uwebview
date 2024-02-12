d314cbcf46110e43ffe491c17569985863279255
Ankit Jain <mejainankit@gmail.com>
2024-02-12 14:08:16 +0530

    Update CordovaGeckoView.js

--- www/CordovaGeckoView.js
+++ www/CordovaGeckoView.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.getGeckoView = function (arg0, success, error) {
-    exec(success, error, 'CordovaGeckoView', 'getGeckoView', [arg0]);
+    exec(success, error, 'CordovaGeckoView', 'loadUrl', [arg0]);
 };
