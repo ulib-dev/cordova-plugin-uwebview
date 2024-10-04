ee8fafc1335010cd5ec41e15cd585129b6aed3fd
u.lib <u.lib@outlook.com>
2024-10-05 02:36:53 +0800

    import

--- src/android/CordovaGeckoView.java
+++ src/android/CordovaGeckoView.java
@@ -8,7 +8,10 @@ import android.util.TypedValue;
 import android.widget.FrameLayout;
 
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.LOG;
 import org.json.JSONArray;
 import org.json.JSONException;
 
