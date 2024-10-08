b9cd6db5cdddd18c1fa37d8effb45cf257305ee3
Warren NG <u.lib@outlook.com>
2024-10-08 15:38:54 +0800

    bak

--- src/android/messaging/content.js
+++ src/android/messaging/content.js
@@ -1,19 +1,18 @@
 console.log(`content:start`);
 let JSBridge = {
-    postMessage: function (message) {
+    postMessage: function(message) {
         browser.runtime.sendMessage({
             action: "JSBridge",
             data: message
         });
     },
-    postMessage2: function (data) {
+    postMessage2: function(data) {
         browser.runtime.sendMessage(data);
     }
 }
 window.wrappedJSObject.JSBridge = cloneInto(
     JSBridge,
-    window,
-    { cloneFunctions: true });
+    window, { cloneFunctions: true });
 
 browser.runtime.onMessage.addListener((data, sender) => {
     console.log("content:eval:" + data);
@@ -36,5 +35,16 @@ browser.runtime.onMessage.addListener((data, sender) => {
         }
         return Promise.resolve(evalCallBack);
     }
-});      // 发送消息到原生应用
- 
+}); // 发送消息到原生应用
+
+
+window.addEventListener("message", function(event) {
+    // We only accept messages from ourselves
+    if (event.source != window)
+        return;
+
+    if (event.data.type && (event.data.type == "FROM_PAGE")) {
+        console.log("Content script received message: " + event.data.text);
+        JSBridge.postMessage2(event.data.data);
+    }
+});
