2268673d1ad75e2bce5328638f9837092c52301b
u.lib <u.lib@outlook.com>
2024-10-10 03:09:20 +0800

    b

--- video-template/src/index.js
+++ video-template/src/index.js
@@ -1,6 +1,6 @@
 import Plyr from "plyr";
 import "../../node_modules/plyr/dist/plyr.css";
- 
+
 // 导入 Font Awesome 的 CSS 文件
 // import "../../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
 // 模拟发送消息到应用
@@ -17,7 +17,7 @@ function uPlayer(newId) {
     hideControls: false, // 设置为 false，控件将不会自动隐藏
     blankVideo: "blank.mp4",
     resetOnEnd: true,
-    // iconUrl: './plyr.svg',
+    iconUrl: "plyr.svg",
     controls: [
       "play-large", // 大的播放按钮
       "play", // 播放/暂停按钮
@@ -199,10 +199,10 @@ function uPlayer(newId) {
       console.log("新视频数据已加载");
       video.play().catch((error) => {
         mySendMessageToApp({
-            event: "changeVideoUrlNew",
-            data: url,
-            s: "直接播放失败",
-          });
+          event: "changeVideoUrlNew",
+          data: url,
+          s: "直接播放失败",
+        });
         console.error("直接播放失败:", error);
       });
     });
