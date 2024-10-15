5c20f3984b60a065c6cb731ed11683cc13826bee
Warren NG <u.lib@outlook.com>
2024-10-15 09:21:41 +0800

    b

--- video-template/src/index-videojs.js
+++ video-template/src/index-videojs.js
@@ -1,7 +1,7 @@
 import videojs from "video.js";
 import "../../node_modules/video.js/dist/video-js.min.css";
 
-const defaultVideoSrc = "resource://android/assets/uvideo/u_video_blank.mp4";
+const defaultVideoSrc = ""; //// "resource://android/assets/uvideo/u_video_blank.mp4";
 let isDefaultVideoLoaded = false; // 标志变量，表示是否尝试加载默认视频
 
 function mySendMessageToApp(message) {
